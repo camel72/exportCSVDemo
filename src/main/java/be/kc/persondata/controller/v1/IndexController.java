acb2a2a49d5cec7b894261c5e9b79e1ffc051898
kc <kristof.camelbeke@smals.be>
2020-09-28 08:22:58 +0200

    configuration thymeleaf /bootstrap #3: adapted index page

--- src/main/java/be/kc/persondata/controller/v1/IndexController.java
+++ src/main/java/be/kc/persondata/controller/v1/IndexController.java
@@ -2,16 +2,20 @@ package be.kc.persondata.controller.v1;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
-    @Value("welcome.message")
-    private String message;
+    @Value("${welcome.message}")
+    private String welcomMessage;
 
     @RequestMapping({"", "/", "index", "index.html"})
-    public String index() {
+    public String index(Model model) {
+        System.out.println("************" + welcomMessage + "/////////////");
+        model.addAttribute("message", welcomMessage);
+
         return "index";
     }
 }
