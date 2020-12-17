b2e24de5599ce11a0e12d1cc5bf401a90922415d
kc <kristof.camelbeke@smals.be>
2020-12-17 19:49:04 +0100

    refactor search

--- src/main/java/be/kc/persondata/controller/v1/SearchFormController.java
+++ src/main/java/be/kc/persondata/controller/v1/SearchFormController.java
@@ -12,10 +12,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 @Controller
 public class SearchFormController {
@@ -37,16 +37,18 @@ public class SearchFormController {
         return "searchForm";
     }
 
-    @PostMapping("/search")
+    @RequestMapping("/search")
     public String searchPersonData(@ModelAttribute PersonData personData, Model model) {
         LOGGER.info("**********searchFrom: searchPersonData *********");
         List<PersonDataDTO> personDataDTOList = null;
-        if (StringUtils.isEmpty(personData.getLastName()) && StringUtils.isEmpty(personData.getFirstName())) {
+        if (isFindAll().test(personData)) {
             personDataDTOList = service.findAllSortedByLastNameFirstNameAndCity();
-        } else if (StringUtils.isNotBlank(personData.getLastName()) && StringUtils.isEmpty(personData.getFirstName())) {
+        } else if (isFindByLastName().test(personData)) {
             personDataDTOList = service.findByLastName(personData.getLastName());
-        } else {
+        } else if (isFindByLastAndFirstName().test(personData)) {
             personDataDTOList = service.findByLastNameAndFirstName(personData.getLastName(), personData.getFirstName());
+        } else if (isFindByStreetAndNumber().test(personData)) {
+            personDataDTOList = service.findByStreetAndNumberAndCity(personData.getStreet(), personData.getNumber(), personData.getCity());
         }
 
         if (personDataDTOList != null) {
@@ -61,5 +63,29 @@ public class SearchFormController {
             LOGGER.info("empty resultset");
         }
         return "searchResult";
+    }
+
+
+    private Predicate<PersonData> isFindAll() {
+        return personData -> StringUtils.isEmpty(personData.getLastName())
+                && StringUtils.isEmpty(personData.getFirstName())
+                && StringUtils.isEmpty(personData.getStreet())
+                && StringUtils.isEmpty(personData.getNumber());
+    }
+
+    private Predicate<PersonData> isFindByLastName() {
+        return personData -> StringUtils.isNotBlank(personData.getLastName())
+                && StringUtils.isEmpty(personData.getFirstName());
+    }
+
+
+    private Predicate<PersonData> isFindByLastAndFirstName() {
+        return personData -> StringUtils.isNotBlank(personData.getLastName())
+                && StringUtils.isNotBlank(personData.getFirstName());
+    }
+
+    private Predicate<PersonData> isFindByStreetAndNumber() {
+        return personData -> StringUtils.isNotBlank(personData.getStreet())
+                && StringUtils.isNotBlank(personData.getNumber());
     }
 }
